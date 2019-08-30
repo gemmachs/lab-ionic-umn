029aa31cb8d088a718d9acbf27f197a699da53f8
gemmachs <gemmacahya@gmail.com>
2019-08-30 15:52:50 +0700

    week03

---
+++ Week03/Week03_GemmaCahyaHafifahSuhengki_00000018209/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [
+//   { path: '', redirectTo: 'home', pathMatch: 'full' },
+//   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+//   { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesPageModule' },
+//   { path: 'recipe-detail', loadChildren: './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule' },
+// ];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  {
+    path: 'recipes',
+      children: [
+      {
+        path: '',
+        loadChildren: './recipes/recipes.module#RecipesPageModule'
+      },
+      {
+        path: ':recipeId',
+        loadChildren: './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule'
+      }
+  ]}];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
